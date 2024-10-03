d33b71ca8c3f1b7f3494d9d3d311a8843a7e7444
TnenntGithub <tanmoychakraborty342@gmail.com>
2024-10-03 23:56:58 +0530

    Fixed

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
+import Image from "next/image";
+import CheckIcon from "@/assets/icons/check-circle.svg";
+import grainImage from '@/assets/images/grain.jpg';
 import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
-import Image from "next/image";
-import CheckIcon from "@/assets/icons/check-circle.svg" 
-import grainImage from '@/assets/images/grain.jpg'                     
 
 const portfolioProjects = [
   {
@@ -11,9 +12,9 @@ const portfolioProjects = [
     year: "2022",
     title: "Dark Saas Landing Page",
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+      { id: 1, title: "Enhanced user experience by 40%" },
+      { id: 2, title: "Improved site speed by 50%" },
+      { id: 3, title: "Increased mobile traffic by 35%" },
     ],
     link: "https://youtu.be/4k7IdSLxh6w",
     image: darkSaasLandingPage,
@@ -23,9 +24,9 @@ const portfolioProjects = [
     year: "2021",
     title: "Light Saas Landing Page",
     results: [
-      { title: "Boosted sales by 20%" },
-      { title: "Expanded customer reach by 35%" },
-      { title: "Increased brand awareness by 15%" },
+      { id: 1, title: "Boosted sales by 20%" },
+      { id: 2, title: "Expanded customer reach by 35%" },
+      { id: 3, title: "Increased brand awareness by 15%" },
     ],
     link: "https://youtu.be/7hi5zwO75yc",
     image: lightSaasLandingPage,
@@ -35,9 +36,9 @@ const portfolioProjects = [
     year: "2023",
     title: "AI Startup Landing Page",
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+      { id: 1, title: "Enhanced user experience by 40%" },
+      { id: 2, title: "Improved site speed by 50%" },
+      { id: 3, title: "Increased mobile traffic by 35%" },
     ],
     link: "https://youtu.be/Z7I5uSRHMHg",
     image: aiStartupLandingPage,
@@ -46,44 +47,60 @@ const portfolioProjects = [
 
 export const ProjectsSection = () => {
   return (
-  <section className="pb-16 lg:py-24">
-    <div className="container">
-      <div className="flex justify-center">
-      <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent text-center bg-clip-text">Tnent Store</p>
+    <section className="pb-16 lg:py-24">
+      <div className="container">
+        <div className="flex justify-center">
+          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent text-center bg-clip-text">
+            Tnent Store
+          </p>
+        </div>
+        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
+          Know More About<br />Tnent Store
+        </h2>
+        <p className="text-center md:text-lg lg:text-xl max-w-md mx-auto text-white/60 mt-4">
+          See how Tnent Store transformed the<br />E-commerce game
+        </p>
+        <div className="flex flex-col mt-10 md:mt-20 gap-20">
+          {portfolioProjects.map((project, index) => (
+            <div key={index} className="bg-gray-800 rounded-3xl relative z-0 after:z-10 overflow-hidden after:content-[''] after:absolute after:inset-0 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 pl-8 pr-8 pt-8 md:pt-12 md:px-10 after:pointer-events-none lg:pt-16 lg:px-20">
+              <div 
+                className="absolute inset-0 -z-10 opacity-5" 
+                style={{
+                  backgroundImage: `url(${grainImage.src})`,
+                }}
+              />
+              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                <div className="lg:pb-16">
+                  <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+                    <span>{project.company}</span>
+                    <span className="mx-2">&bull;</span>
+                    <span>{project.year}</span>
+                  </div>
+                  <h3 className="font-serif md:mt-5 text-2xl md:text-4xl mt-2">{project.title}</h3>
+                  <hr className="border-t-2 border-white/5 md:mt-5 mt-4"/>
+                  <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                    {project.results.map((result) => (
+                      <li key={result.id} className="flex gap-2 text-sm md:text-base text-white/50">
+                        <CheckIcon className="w-5 h-5"/>
+                        <span>{result.title}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="relative">
+                  <Image 
+                    className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none" 
+                    src={project.image} 
+                    alt={project.title}
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-      <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">Know More About<br/>Tnent Store</h2>
-      <p className="text-center md:text-lg lg:text-xl max-w-md mx-auto text-white/60 mt-4">See how Tnent Store transformed the<br/>E-commerce game</p>
-      <div className="flex flex-col mt-10 md:mt-20 gap-20">
-        {portfolioProjects.map(project => (
-          <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 after:z-10 overflow-hidden after:content-[''] after:absolute after:inset-0 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 pl-8 pr-8 pt-8 md:pt-12 md:px-10 after:pointer-events-none lg:pt-16 lg:px-20">
-           <div className="absolute inset-0 -z-10 opacity-5" style={{
-            backgroundImage: `url(${grainImage.src})`,
-           }}
-           ></div>
-           <div className="lg:grid lg:grid-cols-2 lg:gap-16">
-            <div className="lg:pb-16">
-              <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
-              <span>{project.company}</span>
-              <span>&bull;</span>
-              <span>{project.year}</span>
-              </div>
-            <h3 className="font-serif md:mt-5 text-2xl md:text-4xl mt-2">{project.title}</h3>
-            <hr className="border-t-2 border-white/5 md:mt-5 mt-4"/>
-            <ul className="flex flex-col gap-4 mt-4 md:mt-5">
-              {project.results.map((result) => (
-                <li className="flex gap-2 text-sm md:text-base text-white/50">
-                  <CheckIcon className="size-5"/>
-                  <span>{result.title}</span></li>
-              ))}</ul>
-              </div>
-              <div className="relative">
-              <Image className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none" src={project.image} alt={project.title}/>
-              </div>
-              </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  </section>
+    </section>
   );
 };
+
+export default ProjectsSection;
